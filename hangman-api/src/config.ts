77cc5ae10fe1660ae51118b5d76e1a91849ea08c
marina.roca@roche.com <marina.roca@roche.com>
2022-10-13 12:53:44 +0200

    add todo

--- hangman-api/src/config.ts
+++ hangman-api/src/config.ts
@@ -2,6 +2,7 @@ if(process.env.NODE_ENV === 'development') {
   require('dotenv').config();
 }
 
+//TODO: Add description on readme
 export default {
   database: {
     isActive: process.env.DATA_BASE_ACTIVE,
